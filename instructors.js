113bf01677d0ed4c220005e61814a675337a7f31
Daniel Developer <danielfelipedeveloper@gmail.com>
2020-07-23 07:04:05 -0400

    Manipulando dados

--- instructors.js
+++ instructors.js
@@ -1,6 +1,27 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+// SHOW
+
+exports.show = (req, res) => {
+    // req.params.id = /:id
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+    return res.render("instructors/show", { instructor })
+}
 // CREATE
 exports.post = (req, res) => {
 
@@ -11,9 +32,21 @@ exports.post = (req, res) => {
         return res.send('Please, fill all fields')
     }
 
-    req.body.created_at = Date.now()
+    let {avatar_url, birth, name, services, gender} = req.body
+    
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
 
-    data.instructors.push(req.body)
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at,
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send("Write file error!")
